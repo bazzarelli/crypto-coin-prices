fbeb8c8c31c6c4c04722354d2b11005ee4965a46
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2023-03-30 10:49:41 -0700

    clean up index.tsx

---
+++ components/CoinList.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/Home.module.css'
+import CoinButtons from './CoinButtons'
+import type { Coin } from '../lib/types'
+
+export default function CoinList(): React.ReactElement<Coin> {
+    const myCoins = ['bitcoin', 'ethereum', 'cardano', 'aave', 'solana', 'polkadot', 'dogecoin', 'litecoin'];
+
+    return (
+        <div className={styles.list}>
+            <div className={styles.grid}>
+                {myCoins.map((coin) => (
+                    <div key={coin}>
+                        <CoinButtons coin={coin} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react'
 import { ErrorBoundary } from "react-error-boundary";
 // COMPONENTS
 import CoinChartHeading from '../components/CoinChartHeading'
-import CoinButtons from '../components/CoinButtons'
+import CoinList from '../components/CoinList'
 import CoinChart from '../components/CoinChart'
 import HeadTag from '../components/HeadTag'
 import CoinChartError from '../components/CoinChartError'
@@ -11,30 +11,21 @@ import type { Coin } from '../lib/types'
 // STYLES
 import styles from '../styles/Home.module.css'
 
-export default function Home(): React.ReactElement<Coin> {
-  const myCoins = ['bitcoin', 'ethereum', 'cardano', 'aave', 'solana', 'polkadot', 'dogecoin', 'litecoin'];
+export default function Home(): React.ReactElement {
 
   return (
     <>
       <HeadTag />
       <main className={styles.main}>
+
         <CoinChartHeading />
 
         <ErrorBoundary FallbackComponent={CoinChartError}>
           <CoinChart />
         </ErrorBoundary>
 
-
         <Suspense fallback={<h3 className={styles.error}>Fetching crypto spot prices...</h3>}>
-          <div className={styles.list}>
-          <div className={styles.grid}>
-            {myCoins.map((coin) => (
-              <div key={coin}>
-                <CoinButtons coin={coin} />
-              </div>
-            ))}
-          </div>
-          </div>
+          <CoinList />
         </Suspense>
 
       </main>
