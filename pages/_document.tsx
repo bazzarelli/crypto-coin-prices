135f559b71524cf8b6162657d5f8e53947166e3b
BIAGIO AZZARELLI <b.azzarelli@gmail.com>
2026-03-25 19:01:45 -0700

    feat: add apple touch icon for better mobile experience

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/bitcoin-app-icon.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
